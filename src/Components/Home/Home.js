69143f900be72ba853347cb83e8c06c7ad3b297d
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-01 04:17:59 +0600

    Conditional rendering

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,6 +8,20 @@ const Home = () => {
   const [tShirts] = useTShirts();
   const [cart, setCart] = useState([]);
 
+  /*   
+    Conditional rendering===> 
+    1. Element variable
+    2. Ternary operator
+    3. && (shorthand)
+    4. || (shorthand)
+*/
+
+  // // 1. Element variable
+  // let command;
+  // if (cart.length === 0) {
+  //     command = <p>Please add to cart</p>
+  // }
+
   const handleAddToCart = (item) => {
     const exists = cart.find((tShirt) => tShirt._id === item._id);
     if (!exists) {
@@ -35,6 +49,15 @@ const Home = () => {
       </div>
       <div className="cart-container">
         <h1>Selected Items: {cart.length}</h1>
+        {/* {command} */}
+        {/* 2. Ternary operator */}
+        {cart.length <= 5 && cart.length !== 0 ? (
+          <p>Add More items</p>
+        ) : (
+          <p></p>
+        )}
+        {/* 3. && (shorthand)*/}
+        {cart.length === 0 && <p>Please add to cart</p>}
         {cart.map((tShirt) => (
           <Cart
             key={tShirt._id}
@@ -42,6 +65,8 @@ const Home = () => {
             handleRemoveFromCart={handleRemoveFromCart}
           ></Cart>
         ))}
+        {/* 4. || (shorthand)  */}
+        {cart.length < 5 || <button>Remove All</button>}
       </div>
     </div>
   );
