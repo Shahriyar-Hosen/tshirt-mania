f74fb0f4009f6e7d220385a5c513bffa283cbbd9
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-01 00:42:13 +0600

    nav bar add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>This is header page</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='orderreview'>Order Review</Link>
+            </nav>
         </div>
     );
 };
