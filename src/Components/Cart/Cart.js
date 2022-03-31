b48eac3115dd962f7cb51b62bcdd2841c2d53693
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-01 01:29:01 +0600

    home t-shirts & cart container add

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = () => {
+  return (
+    <div>
+      <h1>This is Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is Home page</h1>
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <div className="tShirts-container">
+        <h1>tShirts</h1>
+      </div>
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
