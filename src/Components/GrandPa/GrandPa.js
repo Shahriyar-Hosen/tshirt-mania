e5f422b0353bb4c40cc885ad41e0466a42d8d93a
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-01 16:30:43 +0600

    context api use

--- src/Components/GrandPa/GrandPa.js
+++ src/Components/GrandPa/GrandPa.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import "./GrandPa.css";
 
+export const Spacial = createContext("7");
+
 const GrandPa = () => {
-  const house = 7;
+  const house = "House: " + 7;
+  const gift = "Quran";
   return (
-      <div  className="grand-pa">
+    <Spacial.Provider value={gift}>
+      <div className="grand-pa">
         <h2>Grand pa</h2>
         <p>
           <small>{house}</small>
         </p>
-        <div  style={{ display: "flex" }}>
+        <div style={{ display: "flex" }}>
           <Father house={house}></Father>
           <Uncle house={house}></Uncle>
           <Aunty house={house}></Aunty>
         </div>
       </div>
+    </Spacial.Provider>
   );
 };
 
--- src/Components/Sister/Sister.js
+++ src/Components/Sister/Sister.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Spacial } from '../GrandPa/GrandPa';
 
 const Sister = ({ house }) => {
+    const offer = useContext(Spacial);
     return (
         <div>
             <h1>Sister</h1>
-            <p><small>{ house }</small></p>
+            <p><small>{ house } {offer}</small></p>
+            <p><small>Gift: {offer}</small></p>
         </div>
     );
 };
