99f7fccea27bd7791a6d818b068218f751ddb394
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-01 17:20:21 +0600

    netflix react router 404

--- src/Components/GrandPa/GrandPa.js
+++ src/Components/GrandPa/GrandPa.js
@@ -4,6 +4,18 @@ import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import "./GrandPa.css";
 
+/**
+ * context api
+ * 1. call createContext with a default value
+ * 2. set a variable of the context with uppercase
+ * 3. Make sure you export the context to use it in other places
+ * 4. Wrap you child content using RingContext.Provider
+ * 5. Provide a value
+ * 6. to consume the context from child component
+ * 7. useContext hook and you will you need to pass the contextName
+ * 8. Make sure you take notes...
+ *  */
+
 export const Spacial = createContext("7");
 
 const GrandPa = () => {
